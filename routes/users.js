b007557d11cba21b5c0658da76389de45f12d2c2
redlac <runeman2@gmail.com>
2019-08-17 12:23:28 -0600

    add /users/cool route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool', function(req, res, next){
+    res.write("You're so cool");
+    res.end();
+});
+
 module.exports = router;
